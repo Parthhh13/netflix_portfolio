f1a789d75b3270514f1165b8311fe40c27cbd96d
Parthhh13 <parth.pahwa22@st.niituniversity.in>
2025-07-30 03:22:15 +0530

    Banner fixed

--- src/NetflixTitle.tsx
+++ src/NetflixTitle.tsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react';
 import './NetflixTitle.css';
 import netflixSound from './netflix-sound.mp3';
 import { useNavigate } from 'react-router-dom';
-import logoImage from '../src/images/logo-2.png'; // Update with the path to your logo
 
 const NetflixTitle = () => {
   const [isClicked, setIsClicked] = useState(false);
@@ -26,7 +25,7 @@ const NetflixTitle = () => {
   return (
     <div className="netflix-container" onClick={handlePlaySound}>
       <img 
-        src={logoImage} 
+        src="/images/logo-2.png"
         alt="Custom Logo" 
         className={`netflix-logo ${isClicked ? 'animate' : ''}`} 
       />
--- src/browse/browse.tsx
+++ src/browse/browse.tsx
@@ -2,9 +2,6 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { FaArrowLeft } from 'react-icons/fa';
 import ProfileCard from '../components/ProfileCard';
-import blueImage from '../images/blue.png';
-import greyImage from '../images/grey.png';
-import redImage from '../images/red.png';
 import './browse.css';
 
 const Browse: React.FC = () => {
@@ -13,17 +10,17 @@ const Browse: React.FC = () => {
   const profiles = [
     {
       name: "recruiter",
-      image: blueImage,
+      image: "/images/blue.png",
       backgroundGif: "https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExOTZ5eWwwbjRpdWM1amxyd3VueHhteTVzajVjeGZtZGJ1dDc4MXMyNCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9dg/16u7Ifl2T4zYfQ932F/giphy.gif" // Dark storm clouds
     },
     {
       name: "casual viewer",
-      image: redImage,
+      image: "/images/red.png",
       backgroundGif: "https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExc28yMjMyZmJ6eWtxbmNwdDV6cXk4dWZmcjFhZms2cXBjN2h5ZDJjeSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/QjZXUBUr89CkiWLPjL/giphy.gif" // Dark, abstract digital lights
     },
     {
       name: "developer",
-      image: greyImage,
+      image: "/images/grey.png",
       backgroundGif: "https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExNGNidDl5emZpejY2eGFxa2I4NW0zZGNpbWRlbnBrZ3N2dWhhbzM1MyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/TFPdmm3rdzeZ0kP3zG/giphy.gif" // Flickering neon lights
     },
   ];
